e254ad986fa003fe805b52083e9a13ae2b83ac4f
nicolenohemysanchez <nicolesanches909@gmail.com>
2024-04-02 13:28:17 -0600

    Se creo nuevo formulario

---
+++ FablabCatalagoVirtualCapasUI/EliminarPrototipo.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace FablabCatalagoVirtualCapasUI
+{
+    partial class EliminarPrototipo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // EliminarPrototipo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 417);
+            this.Name = "EliminarPrototipo";
+            this.Text = "EliminarPrototipo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
