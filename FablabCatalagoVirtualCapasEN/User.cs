a8365c3cb591dd1579b5e77b1efb5d0ac5492681
Acosta <osoloco1245@gmail.com>
2024-06-06 00:14:27 -0600

    agregar funciones de crud a los usuarios

--- FablabCatalagoVirtualCapasEN/User.cs
+++ FablabCatalagoVirtualCapasEN/User.cs
@@ -9,7 +9,9 @@ namespace FablabCatalagoVirtualCapasEN
 {
 	public class User
 	{
-		public string Name { get; set; }
+
+		public int Id { get; set; }
+		public string Usuario { get; set; }
 		public string Password { get; set; }
 	}
 }
