9dab3e05a83de34866f429576eb0fb21953e9208
Acosta <osoloco1245@gmail.com>
2024-08-05 22:42:52 -0600

    Se agrego WPF a el proyecto

---
+++ FablabCatalagoVirtualCapasWPF/InicioSesion.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fablab.esfe
+{
+    /// <summary>
+    /// Lógica de interacción para InicioSesion.xaml
+    /// </summary>
+    public partial class InicioSesion : Window
+    {
+        public InicioSesion()
+        {
+            InitializeComponent();
+        }
+
+		private void Window_Closed(object sender, EventArgs e)
+		{
+			this.Close();
+           
+		}
+
+		private void btn_regresar_Click(object sender, RoutedEventArgs e)
+		{
+            var back = new PantallaPrincipal();
+            back.Show();
+            this.Close();
+		}
+	}
+}
