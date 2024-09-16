1d4847effe67c1f219e6a04cf0f70abf22f19900
Acosta <osoloco1245@gmail.com>
2024-09-16 16:25:49 -0600

    Controladores agregados

---
+++ FablabCatalagoVirtualCapasWEB/Controllers/InscripClubsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FablabCatalagoVirtualCapasWEB.Controllers
+{
+    public class InscripClubsController : Controller
+    {
+        // GET: InscripClubs
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FablabCatalagoVirtualCapasWEB/Controllers/SoliProyectosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FablabCatalagoVirtualCapasWEB.Controllers
+{
+    public class SoliProyectosController : Controller
+    {
+        // GET: SoliProyectos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
