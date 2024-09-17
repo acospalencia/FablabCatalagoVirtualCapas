841b74196d885e203622b638dcf5dd98fff44077
Acosta <osoloco1245@gmail.com>
2024-09-17 10:02:51 -0600

    cambios

--- FablabCatalagoVirtualCapasWEB/Controllers/PrototiposController.cs
+++ FablabCatalagoVirtualCapasWEB/Controllers/PrototiposController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FablabCatalagoVirtualCapasBL;
+using FablabCatalagoVirtualCapasEN;
 
 namespace FablabCatalagoVirtualCapasWEB.Controllers
 {
     public class PrototiposController : Controller
     {
+        
         // GET: Prototipos
         public ActionResult Index()
         {
