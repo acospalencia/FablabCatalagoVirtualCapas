7e890212b946e7952c039a42fde053277a878beb
nicolenohemysanchez <nicolesanches909@gmail.com>
2024-09-21 21:50:53 -0600

    cambios

--- FablabCatalagoVirtualCapasWEB/Controllers/ClubsController.cs
+++ FablabCatalagoVirtualCapasWEB/Controllers/ClubsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FablabCatalagoVirtualCapasBL;
+using FablabCatalagoVirtualCapasEN;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +8,47 @@ using System.Web.Mvc;
 
 namespace FablabCatalagoVirtualCapasWEB.Controllers
 {
-    public class ClubsController : Controller
-    {
-        // GET: Clubs
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class ClubsController : Controller
+	{
+		private ClubBL clubsBL = new ClubBL();
+
+		// GET: Clubs
+		public ActionResult Index()
+		{
+			var clubes = clubsBL.RegresarLista();
+			return View(clubes);
+		}
+
+		// GET: Clubs/Details/5
+		public ActionResult Details(int id)
+		{
+			var club = clubsBL.RegresarLista().FirstOrDefault(c => c.Id == id);
+			if (club == null)
+			{
+				return HttpNotFound();
+			}
+			return View(club);
+		}
+
+		// GET: Clubs/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: Clubs/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(Club club)
+		{
+			if (ModelState.IsValid)
+			{
+				clubsBL.GuardarClub(club);
+				return RedirectToAction("Index");
+			}
+			return View(club);
+		}
+
+	
+	}
 }
